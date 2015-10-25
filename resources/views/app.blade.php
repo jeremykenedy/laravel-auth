2cc126559453322766743be705eef1ff2a149cae
jeremykenedy <jeremy@grayboxpdx.com>
2015-10-25 12:15:46 -0700

    added reset password form to laravel html conventions

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -58,5 +58,8 @@
 	<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+	@yield('additional_content')
+
 </body>
 </html>
--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -1,56 +1,64 @@
 @extends('app')
 
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">{{ Lang::get('titles.resetPword') }}</div>
-				<div class="panel-body">
-					@if (session('status'))
-						<div class="alert alert-success">
-							{{ session('status') }}
-						</div>
-					@endif
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
+				<div class="panel panel-default">
+					<div class="panel-heading">{{ Lang::get('titles.resetPword') }}</div>
+					<div class="panel-body">
 
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>{{ Lang::get('auth.whoops') }}</strong> {{ Lang::get('auth.someProblems') }}<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
-
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">{{ Lang::get('auth.email') }}</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+						@if (session('status'))
+							<div class="alert alert-success">
+								{{ session('status') }}
 							</div>
-						</div>
+						@endif
 
-						<div class="g-recaptcha" data-sitekey="{{ env('RE_CAP_SITE') }}"></div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									{{ Lang::get('auth.sendResetLink') }}
-								</button>
+						@if (count($errors) > 0)
+							<div class="form-group">
+								<div class="col-sm-10 col-sm-offset-1">
+									<div class="alert alert-danger">
+										<strong>{{ Lang::get('auth.whoops') }}</strong> {{ Lang::get('auth.someProblems') }}<br><br>
+										<ul>
+											@foreach ($errors->all() as $error)
+												<li>{{ $error }}</li>
+											@endforeach
+										</ul>
+									</div>
+								</div>
 							</div>
-						</div>
-					</form>
+						@endif
+
+						{!! Form::open(array('url' => url('/password/email'), 'method' => 'POST', 'class' => 'lockscreen-credentials form-horizontal', 'role' => 'form')) !!}
+							{!! csrf_field() !!}
+
+							<div class="form-group has-feedback">
+								{!! Form::label('email', Lang::get('auth.email') , array('class' => 'col-sm-4 control-label')); !!}
+								<div class="col-sm-6">
+									{!! Form::email('email', null, array('id' => 'email', 'class' => 'form-control', 'placeholder' => Lang::get('auth.ph_email'), 'required' => 'required',)) !!}
+									<span class="glyphicon glyphicon-envelope form-control-feedback" aria-hidden="true"></span>
+								</div>
+							</div>
+
+							<div class="form-group">
+								<div class="col-sm-6 col-sm-offset-4 ">
+									<div class="g-recaptcha" data-sitekey="{{ env('RE_CAP_SITE') }}"></div>
+								</div>
+							</div>
+
+							<div class="form-group">
+								<div class="col-sm-6 col-sm-offset-4">
+									{!! Form::button(Lang::get('auth.sendResetLink'), array('class' => 'btn btn-primary','type' => 'submit')) !!}
+								</div>
+							</div>
+						{!! Form::close() !!}
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
-
-<script src='https://www.google.com/recaptcha/api.js'></script>
-
 @endsection
 
+@section('additional_content')
+	{!! HTML::script('https://www.google.com/recaptcha/api.js', array('type' => 'text/javascript')) !!}
+@endsection
