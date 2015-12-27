19a0744238c359ac9a14cd88a5bf52115513e02d
jeremykenedy <jeremy@grayboxpdx.com>
2015-12-27 01:05:41 -0800

    calling Google maps api v3 in profiles/edit.blade.php, using it but location id to lookup addresses in google maps

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -96,6 +96,22 @@
 @endsection
 
 @section('template_scripts')
+
+	{!! HTML::script('//maps.googleapis.com/maps/api/js?libraries=places&dummy=.js', array('type' => 'text/javascript')) !!}
+
+	<script type="text/javascript">
+	    //FUNCTION TO ASSIST WITH AUTO ADDRESS INPUT USING GOOGLE MAPS API3
+	    //<![CDATA[
+	    window.onload=function(){
+	        if(document.getElementById("location"))
+	        {
+	            var input = document.getElementById('location');
+	            var options; // = {componentRestrictions: {country: 'us'}};
+	            var autocomplete = new google.maps.places.Autocomplete(input, options);
+	        }
+	     }//]]>
+	</script>
+
 @endsection
 
 
