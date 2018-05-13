0e68edecc3099a9a8f208f570b6e9773933b6eb3
itgoldman <itay@itgoldman.com>
2018-05-13 11:39:41 +0300

    Skipping captchaCheck() altogether if settings.reCaptchStatus is false. Otherwise it fails if doesn't have valid Google reCAPTCHA secret in Environment.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -57,11 +57,11 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
-        $data['captcha'] = $this->captchaCheck();
-
-        if (!config('settings.reCaptchStatus')) {
+        if (config('settings.reCaptchStatus')) {
+            $data['captcha'] = $this->captchaCheck();
+        } else {
             $data['captcha'] = true;
-        }
+		}
 
         return Validator::make($data,
             [
