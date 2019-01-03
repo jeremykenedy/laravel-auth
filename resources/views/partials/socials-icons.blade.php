186194e5d2a12884f65e648971a4d16f3f1ec04a
Kent Dahl <kent@rubendahl.com>
2019-01-03 16:28:48 +0100

    Add missing twitter provider in social icons partial template.

--- resources/views/partials/socials-icons.blade.php
+++ resources/views/partials/socials-icons.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-12 mb-2 text-center">
         {!! HTML::icon_link(route('social.redirect',['provider' => 'facebook']), 'fa fa-facebook', '', array('class' => 'btn btn-social-icon btn-lg mb-1 btn-facebook')) !!}
-        {!! HTML::icon_link(route('social.redirect',['provider' => '']), 'fa fa-twitter', '', array('class' => 'btn btn-social-icon btn-lg mb-1 btn-twitter')) !!}
+        {!! HTML::icon_link(route('social.redirect',['provider' => 'twitter']), 'fa fa-twitter', '', array('class' => 'btn btn-social-icon btn-lg mb-1 btn-twitter')) !!}
         {!! HTML::icon_link(route('social.redirect',['provider' => 'google']), 'fa fa-google-plus', '', array('class' => 'btn btn-social-icon btn-lg mb-1 btn-google')) !!}
         {!! HTML::icon_link(route('social.redirect',['provider' => 'github']), 'fa fa-github', '', array('class' => 'btn btn-social-icon btn-lg mb-1 btn-github')) !!}
         {!! HTML::icon_link(route('social.redirect',['provider' => 'youtube']), 'fa fa-youtube', '', array('class' => 'btn btn-social-icon btn-lg mb-1 btn-youtube')) !!}
