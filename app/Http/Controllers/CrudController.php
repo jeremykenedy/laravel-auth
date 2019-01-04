e521f27ef8c154f46e6a66f05c2cbda5819e2379
oncalldeveloper <oncall.developer@gmail.com>
2019-01-05 01:55:04 +0700

    Adding a CRUD Generator to the admin options, Fixed Code Style Issues

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -36,7 +36,7 @@ class CrudController extends Controller
         // Make sure we do not already have this module
         $directories = glob(base_path().'/resources/views/*', GLOB_ONLYDIR);
         foreach ($directories as $dir){
-            $this_name = str_replace(base_path().'/resources/views/', '',$dir);
+            $this_name = str_replace(base_path().'/resources/views/', '', $dir);
             $used_names[] = $this_name;
         }
 
