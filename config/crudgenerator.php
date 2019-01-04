7b4e28f91294b2ce14d17bcdd283a6e5258f3687
oncalldeveloper <oncall.developer@gmail.com>
2019-01-05 01:47:07 +0700

    Adding a CRUD Generator to the admin options, Fixed Code Style Issues

--- config/crudgenerator.php
+++ config/crudgenerator.php
@@ -15,12 +15,12 @@ return [
 
     'path' => base_path('resources/crud-generator/'),
 
-    /**
+    /*
      * Columns number to show in view's table.
      */
     'view_columns_number' => 3,
 
-    /**
+    /*
      * Delimiter for template vars
      */
     'custom_delimiter' => ['%%', '%%'],
@@ -55,16 +55,16 @@ return [
     |
      */
     'dynamic_view_template' => [
-        'index' => ['formHeadingHtml', 'formBodyHtml', 'crudName', 'crudNameCap', 'modelName', 'viewName', 'routeGroup', 'primaryKey'],
-        'form' => ['formFieldsHtml'],
-        'create' => ['crudName', 'crudNameCap', 'modelName', 'modelNameCap', 'viewName', 'routeGroup', 'viewTemplateDir'],
-        'edit' => ['crudName', 'crudNameSingular', 'crudNameCap', 'modelNameCap', 'modelName', 'viewName', 'routeGroup', 'primaryKey', 'viewTemplateDir'],
-        'show' => ['formHeadingHtml', 'formBodyHtml', 'formBodyHtmlForShowView', 'crudName', 'crudNameSingular', 'crudNameCap', 'modelName', 'viewName', 'routeGroup', 'primaryKey'],
+        'index'  => ['formHeadingHtml', 'formBodyHtml', 'crudName', 'crudNameCap', 'modelName', 'viewName', 'routeGroup', 'primaryKey'],
+        'form'  => ['formFieldsHtml'],
+        'create'  => ['crudName', 'crudNameCap', 'modelName', 'modelNameCap', 'viewName', 'routeGroup', 'viewTemplateDir'],
+        'edit'  => ['crudName', 'crudNameSingular', 'crudNameCap', 'modelNameCap', 'modelName', 'viewName', 'routeGroup', 'primaryKey', 'viewTemplateDir'],
+        'show'  => ['formHeadingHtml', 'formBodyHtml', 'formBodyHtmlForShowView', 'crudName', 'crudNameSingular', 'crudNameCap', 'modelName', 'viewName', 'routeGroup', 'primaryKey'],
         /*
          * Add new stubs templates here if you need to, like action, datatable...
          * custom_template needs to be activated for this to work
          */
-    ]
+    ],
 
 
 ];
