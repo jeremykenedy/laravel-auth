40eddd2b75541d877b488012c79df70a2978d585
Holbi <AkkuFresh@users.noreply.github.com>
2019-07-23 19:31:45 +0200

    Update to the latest 4.3.1 Bootstrap
    All theme links updated.

--- database/seeds/ThemesTableSeeder.php
+++ database/seeds/ThemesTableSeeder.php
@@ -19,79 +19,79 @@ class ThemesTableSeeder extends Seeder
             ],
             [
                 'name' => 'Darkly',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/darkly/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/darkly/bootstrap.min.css',
             ],
             [
                 'name' => 'Cyborg',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/cyborg/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/cyborg/bootstrap.min.css',
             ],
             [
                 'name' => 'Cosmo',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/cosmo/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/cosmo/bootstrap.min.css',
             ],
             [
                 'name' => 'Cerulean',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/cerulean/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/cerulean/bootstrap.min.css',
             ],
             [
                 'name' => 'Flatly',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/flatly/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/flatly/bootstrap.min.css',
             ],
             [
                 'name' => 'Journal',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/journal/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/journal/bootstrap.min.css',
             ],
             [
                 'name' => 'Lumen',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/lumen/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/lumen/bootstrap.min.css',
             ],
             [
                 'name' => 'Paper',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/paper/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/paper/bootstrap.min.css',
             ],
             [
                 'name' => 'Readable',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/readable/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/readable/bootstrap.min.css',
             ],
             [
                 'name' => 'Sandstone',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/sandstone/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/sandstone/bootstrap.min.css',
             ],
             [
                 'name' => 'Simplex',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/simplex/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/simplex/bootstrap.min.css',
             ],
             [
                 'name' => 'Slate',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/slate/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/slate/bootstrap.min.css',
             ],
             [
                 'name' => 'Spacelab',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/spacelab/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/spacelab/bootstrap.min.css',
             ],
             [
                 'name' => 'Superhero',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/superhero/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/superhero/bootstrap.min.css',
             ],
             [
                 'name' => 'United',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/united/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/united/bootstrap.min.css',
             ],
             [
                 'name' => 'Yeti',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/yeti/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/yeti/bootstrap.min.css',
             ],
             [
-                'name' => 'Bootstrap 4.0.0 Alpha',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css',
+                'name' => 'Bootstrap 4.3.1',
+                'link' => 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
             ],
             [
                 'name' => 'Materialize',
-                'link' => 'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css',
+                'link' => 'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.css',
             ],
             [
-                'name' => 'Bootstrap Material Design 4.1.1',
-                'link' => 'https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css',
+                'name' => 'Material Design for Bootstrap (MDB) 4.8.7',
+                'link' => 'https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.7/css/mdb.css',
             ],
             [
                 'name' => 'mdbootstrap',
@@ -99,31 +99,31 @@ class ThemesTableSeeder extends Seeder
             ],
             [
                 'name' => 'Litera',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/litera/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/litera/bootstrap.min.css',
             ],
             [
                 'name' => 'Lux',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/lux/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/lux/bootstrap.min.css',
             ],
             [
                 'name' => 'Materia',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/materia/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/materia/bootstrap.min.css',
             ],
             [
                 'name' => 'Minty',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/minty/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/minty/bootstrap.min.css',
             ],
             [
                 'name' => 'Pulse',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/pulse/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/pulse/bootstrap.min.css',
             ],
             [
                 'name' => 'Sketchy',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/sketchy/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/sketchy/bootstrap.min.css',
             ],
             [
                 'name' => 'Solar',
-                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/solar/bootstrap.min.css',
+                'link' => 'https://maxcdn.bootstrapcdn.com/bootswatch/4.3.1/solar/bootstrap.min.css',
             ],
 
         ];
