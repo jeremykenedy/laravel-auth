1812cfa25509aa0fe542e0686f0eeaf19f76c107
jeremykenedy <jeremykenedy@gmail.com>
2019-07-30 23:22:00 -0700

    enable twitter

--- resources/views/partials/socials.blade.php
+++ resources/views/partials/socials.blade.php
@@ -2,11 +2,9 @@
     <div class="col-sm-6 mb-2">
         {!! HTML::icon_link(route('social.redirect',['provider' => 'facebook']), 'fa fa-facebook', 'Facebook', array('class' => 'btn btn-block btn-social btn-facebook')) !!}
     </div>
-    {{--
     <div class="col-sm-6 mb-2">
         {!! HTML::icon_link(route('social.redirect',['provider' => 'twitter']), 'fa fa-twitter', 'Twitter', array('class' => 'btn btn-block btn-social btn-twitter')) !!}
     </div>
-    --}}
     <div class="col-sm-6 mb-2">
         {!! HTML::icon_link(route('social.redirect',['provider' => 'google']), 'fa fa-google-plus', 'Google +', array('class' => 'btn btn-block btn-social btn-google')) !!}
     </div>
