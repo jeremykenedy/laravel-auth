a36624f660fb8cc279fec7ae09a93e24ac546988
Tom White <tompwhite@yahoo.com.au>
2019-09-26 12:11:10 +1000

    Update SocialController.php

--- app/Http/Controllers/Auth/SocialController.php
+++ app/Http/Controllers/Auth/SocialController.php
@@ -9,7 +9,7 @@ use App\Models\User;
 use App\Traits\ActivationTrait;
 use App\Traits\CaptureIpTrait;
 use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Input;
+use Illuminate\Http\Request;
 use jeremykenedy\LaravelRoles\Models\Role;
 use Laravel\Socialite\Facades\Socialite;
 
@@ -43,9 +43,9 @@ class SocialController extends Controller
      *
      * @return Redirect
      */
-    public function getSocialHandle($provider)
+    public function getSocialHandle($provider, Request $request)
     {
-        if (Input::get('denied') != '') {
+        if ($request->get('denied') != '') {
             return redirect()->to('login')
                 ->with('status', 'danger')
                 ->with('message', trans('socials.denied'));
